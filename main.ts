ceea9daf518144dbc253889075137845c3b07a6c
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-02-04 11:44:37 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
-OLED.init(128, 64)
-basic.forever(function () {
+function showDistanceAndClue () {
+    distance = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P1)
     OLED.clear()
     OLED.newLine()
-    OLED.writeStringNewLine("" + sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P1) + " mm")
-    basic.pause(500)
+    if (distance > 110 && distance < 130) {
+        OLED.writeStringNewLine("Clue #5")
+    }
+    if (distance > 90 && distance < 109) {
+        OLED.writeStringNewLine("Clue #4")
+    }
+    if (distance > 60 && distance < 89) {
+        OLED.writeStringNewLine("Clue #3")
+    }
+    if (distance > 40 && distance < 59) {
+        OLED.writeStringNewLine("Clue #2")
+    }
+    if (distance > 25 && distance < 39) {
+        OLED.writeStringNewLine("Clue #1")
+    }
+    OLED.newLine()
+    OLED.writeStringNewLine("" + distance + " mm")
+}
+let distance = 0
+OLED.init(128, 64)
+distance = 0
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    showDistanceAndClue()
+    basic.pause(2000)
 })
